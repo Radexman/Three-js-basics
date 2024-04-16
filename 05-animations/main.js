205199011ed5f1ac3f67641e34cab35264668a4e
Radexman <Borderlandsmaniak@gmail.com>
2024-04-16 23:42:21 +0200

    gsap

--- 05-animations/main.js
+++ 05-animations/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const canvas = document.querySelector('.webgl');
 
@@ -26,14 +27,19 @@ renderer.setSize(sizes.width, sizes.height);
 // Clock
 const clock = new THREE.Clock();
 
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
 // Animations
 const tick = () => {
 	// Click
-	const elapsedTime = clock.getElapsedTime();
-	console.log(elapsedTime);
+	// const elapsedTime = clock.getElapsedTime();
 
 	// Update objects
-	mesh.rotation.y = elapsedTime;
+	// mesh.rotation.y = elapsedTime * Math.PI * 0.5;
+	// camera.position.y = Math.sin(elapsedTime);
+	// camera.position.x = Math.cos(elapsedTime);
+	// camera.lookAt(mesh.position);
 
 	// Render
 	renderer.render(scene, camera);
