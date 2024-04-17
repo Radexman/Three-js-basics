5cd34b912a9c4c5bd8ad7a24150052a27c3a7c83
Radexman <Borderlandsmaniak@gmail.com>
2024-04-17 17:51:51 +0200

    tweens

--- 05-animations/main.js
+++ 05-animations/main.js
@@ -5,8 +5,8 @@ const canvas = document.querySelector('.webgl');
 
 const scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
-mesh.position.set(0, 0, 0);
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+mesh.scale.set(2, 0.3, 1);
 scene.add(mesh);
 
 const sizes = {
@@ -15,7 +15,7 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(0, 0, 3);
+camera.position.set(0, 0, 4);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -24,27 +24,25 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-// Clock
+// Anomations
+
+gsap.to(mesh.position, { duration: 3, x: 1 });
+gsap.to(mesh.position, { delay: 6, duration: 5, y: 2 });
+gsap.to(mesh.position, { delay: 11, duration: 3, x: -1 });
+gsap.to(mesh.position, { delay: 14, duration: 5, x: -1, y: 0 });
+gsap.to(mesh.position, { delay: 19, duration: 1, x: 0, y: 0 });
+
 const clock = new THREE.Clock();
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
-
-// Animations
-const tick = () => {
-	// Click
-	// const elapsedTime = clock.getElapsedTime();
-
-	// Update objects
-	// mesh.rotation.y = elapsedTime * Math.PI * 0.5;
-	// camera.position.y = Math.sin(elapsedTime);
-	// camera.position.x = Math.cos(elapsedTime);
+const animateMesh = () => {
+	const elapsedTime = clock.getElapsedTime();
+	mesh.rotation.x = elapsedTime * Math.PI * 0.5;
+	// mesh.position.x = Math.sin(elapsedTime);
+	// mesh.position.y = Math.cos(elapsedTime);
+	// camera.position.y = Math.cos(elapsedTime);
 	// camera.lookAt(mesh.position);
-
-	// Render
 	renderer.render(scene, camera);
-
-	requestAnimationFrame(tick);
+	requestAnimationFrame(animateMesh);
 };
 
-tick();
+animateMesh();
