e9441ebecff8fc4cf485981d2ece597fa8ee2e0a
Radexman <Borderlandsmaniak@gmail.com>
2024-04-18 18:04:06 +0200

    Fullscreen and resizing

--- 07-fullscreen-and-resizing/main.js
+++ 07-fullscreen-and-resizing/main.js
@@ -2,10 +2,12 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const canvas = document.querySelector('.webgl');
+
 const sizes = {
 	width: innerWidth,
 	height: innerHeight,
 };
+
 const coursor = {
 	x: 0,
 	y: 0,
@@ -14,6 +16,36 @@ const coursor = {
 window.addEventListener('mousemove', (e) => {
 	coursor.x = e.clientX / sizes.width - 0.5;
 	coursor.y = -(e.clientY / sizes.height - 0.5);
+});
+
+window.addEventListener('dblclick', () => {
+	const fullscreenElement = document.fullscreenElement || document.webkitFullScreenElement;
+	if (!fullscreenElement) {
+		if (canvas.requestFullscreen) {
+			canvas.requestFullscreen();
+		} else if (canvas.webkitRequestFullScreen) {
+			canvas.webkitRequestFullScreen();
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.webkitExitFullScreen) {
+			document.webkitExitFullScreen();
+		}
+	}
+});
+
+window.addEventListener('resize', () => {
+	// Update sizes
+	sizes.width = innerWidth;
+	sizes.height = innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
 });
 
 const scene = new THREE.Scene();
@@ -31,15 +63,17 @@ controls.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
 	canvas,
+	antialias: true,
 });
 
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const clock = new THREE.Clock();
 
 const animate = () => {
 	const elapsedTime = clock.getElapsedTime();
-	// mesh.rotation.y = elapsedTime * Math.PI * 0.25;
+	mesh.rotation.y = elapsedTime * Math.PI * 0.25;
 	// camera.position.x = Math.sin(coursor.x * Math.PI * 2) * 3;
 	// camera.position.z = Math.cos(coursor.x * Math.PI * 2) * 3;
 	// camera.position.y = coursor.y * 5;
